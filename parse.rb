75c4909f1dd1a90239a5ddd6b0b6924fe02efff3
ShoginCoffee <cg.co.gu@gmail.com>
2022-12-05 10:16:40 +0100

    added parse function

---
+++ parse.rb
@@ -0,0 +1,20 @@
+require 'pp'
+
+def parse_request(request_string)
+    first_line = request_string.split("\n").first.split(" ")
+    headers = request_string.split("\n").drop(1).map { |row| row.split(": ") }.to_h
+    request = { 'verb': first_line[0], 'resource': first_line[1], 'headers': headers }
+
+    return request
+end
+
+request_string = <<~END
+GET /hello HTTP/1.1
+User-Agent: Mozilla/4.0 (compatible; MSIE5.01; Windows NT)
+Host: www.tutorialspoint.com
+Accept-Language: en-us
+Accept-Encoding: gzip, deflate
+Connection: Keep-Alive
+END
+
+pp parse_request(request_string)
